dce87d490cb95456c41d5edb92009bbb1c38c4dd
Moon1705 <t.k.n-24@mail.ru>
2020-12-21 10:29:32 +0300

    Add func length and size

--- module/store/data_store.py
+++ module/store/data_store.py
@@ -1,17 +1,31 @@
+import sys
+
 class DataStore:
 
     def __init__(self, store=set()) -> None:
-        self.store = store
+        self.__store = store
 
     def write(self, event) -> None:
-        self.store.add(event)
+        self.__store.add(event)
 
     def read_all(self) -> set:
-        return self.store
+        return self.__store
 
     def delete_all(self) -> None:
-        self.store.clear()
+        self.__store.clear()
 
     def check_count_element(self, count) -> bool:
-        return True if (len(self.store >= count)) else False
+        return True if (len(self.__store >= count)) else False
+
+    def size(self) -> str:
+        size_store = sys.getsizeof(self.__store)
+        if size_store < 1024:
+            return f"{size_store} B"
+        elif 1024 <= size_store < (1024 * 1024):
+            return f"{round(size_store / 1024, 2)} Kb"
+        else:
+            return f"{round(size_store / (1024 * 1024), 2)} Mb"
+    
+    def length(self) -> int:
+        return len(self.__store)
 
