d3ffac80ef534f3c74c76000b41ebc87d409b535
Moon1705 <t.k.n-24@mail.ru>
2020-12-21 10:25:45 +0300

    Rename module Sniff to Sniffer

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import time
-from module.sniff.sniffer import Sniff
+from module.sniff.sniffer import Sniffer
 from module.store.data_store import DataStore
 
 def main():
     data_store = DataStore()
     sniff = Sniffer(interface='eth0', store=data_store)
     sniff.start()
-    time.sleep(100)
+    time.sleep(30)
     sniff.stop()
 
 
